acad312d52a7a760e489eec169829f76679dc040
haidang71214 <haidang71214@gmail.com>
2026-07-23 09:40:18 +0700

    refactor(utils): optimize jwt token helper and crypto utilities

--- utils/token.module.ts
+++ utils/token.module.ts
@@ -2,12 +2,9 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { TokenControllerService } from './token.utils';
 
-
 @Module({
-  imports:[
-     JwtModule.register({})  ],
+  imports: [JwtModule.register({})],
   providers: [TokenControllerService],
-  exports:[TokenControllerService]
-  
+  exports: [TokenControllerService],
 })
 export class TokenModule {}
--- utils/token.utils.ts
+++ utils/token.utils.ts
@@ -1,11 +1,11 @@
 import * as jwt from 'jsonwebtoken';
 
-export class TokenControllerService{
+export class TokenControllerService {
   createTokenAsyncKey = (payload: any): string => {
-  const privateKey = process.env.PRIVATE_KEY || 'Dang-deptrai-vcl';
-  return jwt.sign({ data: payload }, privateKey, {
-    algorithm: 'RS256',
-    expiresIn: '7d', 
-  });
-};
+    const privateKey = process.env.PRIVATE_KEY || 'Dang-deptrai-vcl';
+    return jwt.sign({ data: payload }, privateKey, {
+      algorithm: 'RS256',
+      expiresIn: '7d',
+    });
+  };
 }
