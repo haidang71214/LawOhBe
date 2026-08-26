a4d7ca2745dfe6c72b1280df07af529e389c7d78
haidang71214 <haidang71214@gmail.com>
2026-08-26 21:25:20 +0700

    feat(storage): configure multer and large file upload limits

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,30 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { setupSocketIo } from './config/socket-io';
+import { ChatService } from './message/message.service';
+import { CommentService } from './comment/comment.service';
 
+
+export const URL_PRODUCTION = 'https://lawohfe.onrender.com'
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
-    const configService = app.get(ConfigService); 
-    
+    const configService = app.get(ConfigService);
+
+    // Cấu hình global pipes
     app.useGlobalPipes(new ValidationPipe());
+ 
+    // Cấu hình CORS
     app.enableCors({
-      origin: ['http://localhost:3000'],
+      //  deploy fe, thay đổi những thứ có ở cái localhost
+      // gán cái fe vào đây
+      origin: [process.env.NODE_ENV === 'production' ? `${URL_PRODUCTION}` : '*','http://localhost:3000'],
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      credentials: true, // cho phép gửi cookie
+      credentials: true,
     });
 
+    // Cấu hình Swagger
     const configSwagger = new DocumentBuilder()
       .setTitle('API LAWOH')
       .setDescription('LAWOH API LIST')
@@ -25,14 +36,22 @@ async function bootstrap() {
 
     const swagger = SwaggerModule.createDocument(app, configSwagger);
     SwaggerModule.setup('Swagger', app, swagger);
-    
-    const port = configService.get<number>('PORT') || 8080;
-    await app.listen(port);
 
-    console.log(`Application is running on: http://localhost:${port}`);
-    console.log(`MongoDB URI: ${configService.get<string>('MONGODB_URI')}`);
+    // Lấy port từ process.env.PORT (Render) hoặc ConfigService
+    // const port = process.env.PORT || configService.get<number>('PORT') || 8080;
+
+    // Khởi tạo và setup Socket.io
+    const chatService = app.get(ChatService);
+    const commentService = app.get(CommentService);
+    setupSocketIo(app, chatService, commentService);
+
+    // Khởi động ứng dụng trên 0.0.0.0 kệ mẹ cái env vì env đéo chạy được
+    const port = process.env.PORT || 10000; // Mặc định 10000 theo tài liệu Render
+await app.listen(port, '0.0.0.0');
+console.log(`Ứng dụng đang chạy trên port: http://0.0.0.0:${port}`);
   } catch (error) {
-    console.error('Application failed to start:', error);
+    console.error('Ứng dụng không khởi động được:', error.stack);
+    process.exit(1);
   }
 }
 
